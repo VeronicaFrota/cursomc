b83f1bd0dc56b3bb36fdfe3915307611d8a16150
Veronica Frota <veronica.s.frota@gmail.com>
2019-01-24 20:51:54 -0200

    Refrash Token

---
+++ src/main/java/com/veronicafrota/cursomc/resource/AuthResource.java
@@ -0,0 +1,30 @@
+package com.veronicafrota.cursomc.resource;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.veronicafrota.cursomc.security.JWTUtil;
+import com.veronicafrota.cursomc.security.UserSS;
+import com.veronicafrota.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
